3c127e4be825603a6017948b05fd29485eda7e76
rpek_ <107241754+yager008@users.noreply.github.com>
2024-05-07 20:19:27 +0300

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,83 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+vim.cmd("language en_US")
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- Shorthand notation for GitHub; translates to https://github.com/junegunn/vim-easy-align
+Plug('junegunn/vim-easy-align')
+
+-- Any valid git URL is allowed
+Plug('https://github.com/junegunn/seoul256.vim.git')
+
+-- Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
+Plug('fatih/vim-go', { ['tag'] = '*' })
+
+-- Using a non-default branch
+Plug('neoclide/coc.nvim', { ['branch'] = 'release' })
+
+-- Use 'dir' option to install plugin in a non-default directory
+Plug('junegunn/fzf', { ['dir'] = '~/.fzf' })
+
+-- Post-update hook: run a shell command after installing or updating the plugin
+Plug('junegunn/fzf', { ['dir'] = '~/.fzf', ['do'] = './install --all' })
+
+-- Post-update hook can be a lambda expression
+Plug('junegunn/fzf', { ['do'] = function()
+  vim.fn['fzf#install']()
+end })
+
+-- If the vim plugin is in a subdirectory, use 'rtp' option to specify its path
+Plug('nsf/gocode', { ['rtp'] = 'vim' })
+
+-- On-demand loading: loaded when the specified command is executed
+Plug('preservim/nerdtree', { ['on'] = 'NERDTreeToggle' })
+
+-- On-demand loading: loaded when a file with a specific file type is opened
+Plug('tpope/vim-fireplace', { ['for'] = 'clojure' })
+
+-- Unmanaged plugin (manually installed and updated)
+Plug('~/my-prototype-plugin')
+
+vim.call('plug#end')
